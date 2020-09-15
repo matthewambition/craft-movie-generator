2d6c7c970de203656fdae0da87b49c4f3aff7134
Matt Crane <matt@ambitioncreative.co.uk>
2020-09-15 10:10:31 +0100

    Initial Commit

---
+++ config/general.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+return [
+    // Global settings
+    '*' => [
+        'defaultWeekStartDay' => 1,
+        'omitScriptNameInUrls' => true,
+        'cpTrigger' => 'admin',
+        'securityKey' => getenv('SECURITY_KEY'),
+        'useProjectConfigFile' => false,
+        'doAnalytics' => false,
+        'gaCode' => 'GTM-XXXXXXX',
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        'devMode' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        'allowAdminChanges' => true,
+        'devMode' => true,
+    ],
+
+    // Production environment settings
+    'production' => [
+        'allowAdminChanges' => false,
+        'doAnalytics' => true,
+    ],
+];
---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('./web/assets');
+
+mix.combine([
+	'node_modules/jquery/dist/jquery.js',
+	'node_modules/popper.js/dist/umd/popper.min.js',
+	'node_modules/bootstrap/dist/js/bootstrap.min.js',
+	'node_modules/slick-carousel/slick/slick.min.js',
+	],
+	'web/assets/js/vendor.js');
+
+mix.js('src/js/app.js', 'web/assets/js')
+mix.sass('src/sass/app.scss', 'web/assets/css')
+
+mix.version();
+mix.disableNotifications();
